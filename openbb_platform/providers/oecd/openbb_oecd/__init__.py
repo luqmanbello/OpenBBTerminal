248d3afc74f766814916280dd1636c678e6fd113
James Maslek <jmaslek11@gmail.com>
2024-01-30 19:34:09 -0500

    Add OECD unemployment (+more) + data cache idea (#5917)
    * Add OECD unemployment + data cache idea
    
    * Docstring edits
    
    * Tests
    
    * Add composite leading indicator
    
    * i truested ruff --fix too much
    
    * Add short + long term interest rates
    
    * pylint
    
    * pylint2
    
    * was not committing.  my b
    
    * add csv cache + make it default.  remove pyarrow from poetry
    
    * Clean up
    
    * Didnt run on this file.  My bad
    
    * Why was this ignored /shrug
    
    * pylint
    
    ---------
    
    Co-authored-by: Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>

--- openbb_platform/providers/oecd/openbb_oecd/__init__.py
+++ openbb_platform/providers/oecd/openbb_oecd/__init__.py
@@ -1,17 +1,14 @@
-<<<<<<< HEAD
 """OECD provider module."""
-=======
-"""FRED provider module."""
-<<<<<<< HEAD
->>>>>>> 13283fbfce (CI listing quick fix (#6002))
-=======
->>>>>>> 13283fbfce (CI listing quick fix (#6002))
 
 from openbb_core.provider.abstract.provider import Provider
+from openbb_oecd.models.composite_leading_indicator import OECDCLIFetcher
 from openbb_oecd.models.composite_leading_indicator import OECDCLIFetcher
 from openbb_oecd.models.gdp_forecast import OECDGdpForecastFetcher
 from openbb_oecd.models.gdp_nominal import OECDGdpNominalFetcher
 from openbb_oecd.models.gdp_real import OECDGdpRealFetcher
+from openbb_oecd.models.long_term_interest_rate import OECDLTIRFetcher
+from openbb_oecd.models.short_term_interest_rate import OECDSTIRFetcher
+from openbb_oecd.models.unemployment import OECDUnemploymentFetcher
 from openbb_oecd.models.long_term_interest_rate import OECDLTIRFetcher
 from openbb_oecd.models.short_term_interest_rate import OECDSTIRFetcher
 from openbb_oecd.models.unemployment import OECDUnemploymentFetcher
@@ -24,6 +21,10 @@ oecd_provider = Provider(
         "GdpNominal": OECDGdpNominalFetcher,
         "GdpReal": OECDGdpRealFetcher,
         "GdpForecast": OECDGdpForecastFetcher,
+        "Unemployment": OECDUnemploymentFetcher,
+        "CLI": OECDCLIFetcher,
+        "STIR": OECDSTIRFetcher,
+        "LTIR": OECDLTIRFetcher,
         "Unemployment": OECDUnemploymentFetcher,
         "CLI": OECDCLIFetcher,
         "STIR": OECDSTIRFetcher,
